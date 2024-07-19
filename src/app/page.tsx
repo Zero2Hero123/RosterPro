1a0a0813f3f40b79441c3ffaac7b374e0bb02469
Hero Emenalom <heroemenalom73@gmail.com>
2024-07-19 10:22:04 -0400

    add showcase video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className=" flex justify-center flex-col items-center">
-      <section className="h-[40vh] max-h-[] flex items-center ">
-        <span className="text-5xl font-bold text-center">Empowering Businesses with Intelligent Scheduling.</span>
+    <main className=" flex justify-center flex-col items-center pb-20">
+      <section className="h-[30vh] max-h-[] flex flex-col justify-center items-center ">
+        <span className="text-4xl font-bold text-center">Empowering Businesses with Intelligent Scheduling.</span>
+        <span>...for free!</span>
       </section>
-      <Link href='/dashboard/scheduler'>Click here</Link>
+
+      <section className="h-20">
+        <Link href='/auth'>
+          <Button className="bg-white text-black hover:bg-slate-300 hover:text-black">Use RosterPro</Button>
+        </Link>
+      </section>
+
+      <video className="w-[90%] shadow-2xl shadow-slate-500 hover:cursor-pointer" loop autoPlay muted>
+        <source src='/showcase.mov' type="video/mp4" />
+      </video>
+      
+      
     </main>
   );
 }
