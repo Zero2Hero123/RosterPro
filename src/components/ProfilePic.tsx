0bc835f05a473cb66ab6b841465a1b0ff4cdf9cf
Hero Emenalom <heroemenalom73@gmail.com>
2024-11-07 17:19:02 -0500

    implement basic chat functionality

--- src/components/ProfilePic.tsx
+++ src/components/ProfilePic.tsx
@@ -1,22 +1,18 @@
 
 
-
-
-
-
 interface Props {
     firstName: string;
     lastName: string;
-    size: number
+    size?: number
 }
 
 
-export default async function ProfilePic({firstName,lastName,size}: Props){
+export default function ProfilePic({firstName,lastName,size}: Props){
 
     return <>
     
-        <div className={`rounded-full flex justify-center items-center md:h-20 md:w-20 h-12 w-12 bg-gradient-to-tr from-blue-600 to-blue-900 p-3 hover:cursor-pointer mx-1`}>
-            <span className=" font-medium text-2xl md:text-4xl">{getInitials(firstName,lastName)}</span>
+        <div className={`rounded-full flex justify-center items-center ${size ? `w-${size} h-${size}`: 'md:h-20 md:w-20 h-12 w-12' } bg-gradient-to-tr from-blue-600 to-blue-900 p-3 hover:cursor-pointer mx-1`}>
+            <span className={`font-medium ${size ? `font-medium text-xl` : 'text-2xl md:text-4xl'}`}>{getInitials(firstName,lastName)}</span>
         </div>
     
     </>
--- src/components/dashboard-ui/chat/Message.tsx
+++ src/components/dashboard-ui/chat/Message.tsx
@@ -1,3 +1,7 @@
+import ProfilePic from "@/components/ProfilePic";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 
 
@@ -8,17 +12,45 @@ interface Props {
     businessId: string;
     authorId: string;
     createdAt: Date;
+    withPersonLabel?: boolean;
 }
 
 
 
-const Message = ({content,authorId}: Props) => {
+const Message = ({content,authorId, withPersonLabel = true}: Props) => {
 
-    return <div className="flex justify-start">
+    const supabase = createClient()
 
-        <div className="bg-slate-800 text-white p-3 rounded-lg text-sm">
-            <span>{authorId}</span>
-            <span>{content}</span>
+    const [user,setUser] = useState<User | null>(null) // current user logged in
+
+    const [author,setAuthor] = useState<any>(null)
+
+    useEffect(() => {
+        supabase.from('profiles') // get the author of the current message
+            .select()
+            .eq('id',authorId)
+            .then(res => {
+                if(!res.error){
+                    setAuthor(res.data[0])
+                } else {
+                    console.error(res.error.message)
+                }
+            })
+    },[])
+
+    useEffect(() => {
+        supabase.auth.getUser()
+            .then(res => setUser(res.data.user))
+    },[])
+
+    return <div className={`flex ${authorId == user?.id ? 'justify-end' : 'justify-start'}`}>
+
+        <div className="flex bg-slate-800 text-white p-3 rounded-lg text-sm">
+            {(withPersonLabel && author) && <ProfilePic firstName={author.first_name} lastName={author.last_name} size={10}/>}
+            <div className="flex flex-col">
+                {(withPersonLabel && author) && <span>{author.first_name} {author.last_name}</span>}
+                <span>{content}</span>
+            </div>
         </div>
 
     </div>
