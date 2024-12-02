3f8c34f411158bc1d0b6232282f1126416705312
Hero Emenalom <heroemenalom73@gmail.com>
2024-12-02 15:14:45 -0500

    enable cell editing

--- src/components/shift-table-ui/Cell.tsx
+++ src/components/shift-table-ui/Cell.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface Props {
     children?: ReactNode
@@ -8,9 +8,38 @@ interface Props {
 
 const Cell: React.FC<Props> = ({children}) => {
 
+    const [isEditing,setIsEditing] = useState(false)
+    const userInput = useRef<HTMLInputElement>(null)
+
+    const [content,setContent] = useState(children?.toString().replaceAll(',','')) 
+
+    useEffect(() => {
+        if(isEditing){
+            if(userInput.current){
+                userInput.current.focus()
+            }
+        }
+
+        if(userInput.current){
+            userInput.current.addEventListener('focusout', () => {
+                setIsEditing(false)
+            })
+        }
+
+        return () => {
+            if(userInput.current){
+                userInput.current.removeEventListener('focusout', () => {
+                    setIsEditing(false)
+                })
+            }
+        }
+    },[isEditing])
+
     // data-swapy-slot={index}
 
-    return <div className="border text-black flex justify-center">{children}</div>
+    return <div onClick={() => setIsEditing(true)} className="hover:cursor-text border text-black flex items-center justify-center h-6">
+        {isEditing ? <input onKeyDown={(k) => k.code === 'Enter' && setIsEditing(false) } placeholder={content} ref={userInput} onChange={(e) => setContent(e.target.value)} className="grow w-16 h-6 focus:outline-dashed p-0 m-0 text-center" type="text"/> : content}
+    </div>
 }
 
 
